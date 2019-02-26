b9187b401859907f005a682d01de808483b2689a
saidul-islam <tuhinmd718@gmail.com>
2019-02-26 21:43:01 +0600

    Download images from urls list

--- images_downloader.py
+++ images_downloader.py
@@ -45,6 +45,67 @@ def file_read(file_path):
 	return urls
 
 
+def image_downloader_func(input_file_path='', save_images_path=''):
+	"""
+	Download image in JPEG format from given links.
+
+	:param input_file_path: Text file path where image link stored
+	:param output_file_path: Folder path where downloaded image will be saved.
+	"""
+
+	if input_file_path:
+		input_file_path = Path(input_file_path)
+	else:
+		input_file_path = Path.cwd().joinpath('images.txt')
+
+
+	if save_images_path:
+		save_images_path = Path(save_images_path)
+	else:
+		save_images_path = Path.cwd().joinpath('Images')
+		
+
+	image_urls = file_read(input_file_path)
+	print(image_urls)
+
+
+	if image_urls:
+		total_urls = len(image_urls)
+
+		try:
+			save_images_path.mkdir(exist_ok=True)
+		
+		except OSError as e:
+			logger.error(e)
+
+		for count, link in enumerate(image_urls, 1):
+			try:
+				img_req = requests.get(link, allow_redirects=False, timeout=(5,15))
+				
+				if img_req.status_code == 200:	
+					img_req.raw.decode_content = True
+					buffer_content = io.BytesIO(img_req.content) # IMPORTANT: Convert byte stream before passing Image.open()
+					img = Image.open(buffer_content)
+
+					img_file_name = Path(link).stem + '.jpeg'
+					img_full_path = save_images_path.joinpath(img_file_name)
+
+					img.save(img_full_path)
+					logger.debug("Download and save image:{}; Remain: {}".format(img_file_name,total_urls-count))
+
+				else:
+					logger.error("URL:{} > Response Code: {}".format(link,img_req.status_code))
+			
+			except (ReadTimeout, ConnectTimeout, HTTPError, Timeout, ConnectionError) as img_req_e:
+				logger.error(img_req_e)
+			
+			except Exception as e:
+				logger.error(e)
+	
+	else:
+		logger.debug("No image url found in {}".format(input_file_path))
+
+
 if __name__ == '__main__':
-    urls = file_read(Path.cwd().joinpath('images.txt'))
-    print(urls)
+	
+	image_downloader_func()
